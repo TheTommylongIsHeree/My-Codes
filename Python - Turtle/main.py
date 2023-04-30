0ba4d103e204745db2c5e1344446204542cb71ff
TheTommylongIsHeree <nguyenlychaulong@gmail.com>
2023-04-30 16:05:57 +0700

    modified:   Python - Turtle/main.py

--- Python - Turtle/main.py
+++ Python - Turtle/main.py
@@ -13,15 +13,14 @@ while a in range (361):
     t.rt(1)
     a = a + 1
 print("The turtle has completed in drawing the circle with a radius of", radius)
-time.sleep(5)
+pause()
 print("Setting up to draw the triangle. Please wait...")
 a = 1
 t.lt(30)
 t.rt(90)
 t.pd()
 b = 1
-print("Set up completed successfully, drawing the filled triangle in a few seconds...")
-time.sleep(5)
+pause("Set up completed successfully, drawing the filled triangle when any key is pressed")
 while b == 1:
     t.fd(a)
     t.rt(120)
@@ -31,11 +30,10 @@ while b == 1:
     t.rt(120)
     print("The turtle has complete in drawing a triangle with a of" , a,", a of the next triangle is", a + 1)
     a = a + 1
-    time.sleep(0.5)
+    time.sleep(0.25)
     if a == 175 or a >> 175:
         print("The turtle has complete in drawing a filled triangle with a of" , a)
         b = 0
-warnedtimes = 1
 pause("PROGRAM STOPPED, PRESS ANY KEY TO EXIT")
 exit()
 time.sleep(5)
@@ -50,7 +48,7 @@ else:
     os.system("killall py")
 time.sleep(10)
 print("i give up, time to reboot your pc lol")
-time.sleep(10)
+time.sleep(5)
 if plt == "Windows":
     os.system("shutdown -t 0 -r -f")
 else:
